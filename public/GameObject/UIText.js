6322e7aa8555bd2dfc5500d868896b69b6a9dd7a
Louxml <1764097857@qq.com>
2020-09-08 10:15:07 +0800

    [feat]添加UIComponent

--- public/GameObject/UIText.js
+++ public/GameObject/UIText.js
@@ -1,4 +1,4 @@
-class UIText extends Component{
+class UIText extends UIComponent{
 
     constructor(text=''){
         super();
@@ -13,199 +13,34 @@ class UIText extends Component{
         this.radius = 0;
         this.align = "left";
         this.alpha = 1;
+        this.weight = 500;
         this.overflow = false;
-        // this.left = "50%";
-        // this.top = 100;
-        // this.anchor = new Vector(0.5,0.5);
+        this.borderBottom = "";
+        this.boxShadow = "";
+        this.cursor = "pointer";
+        this.left = "";
+        this.top = "";
+        this.anchor = new Vector(0,0);
+        this.zoom = 0;
     }
 
-    set width(value){
-        this.html.style.width = value+"px";
+    Onclick(e){
+        
     }
-    get width(){
-        return this.html.style.width;
-    }
-    set height(value){
-        this.html.style.height = value+"px";
-    }
-    get height(){
-        return this.html.style.height;
-    }
-    set text(value){
-        this.html.innerText = value;
-    }
-    get text(){
-        return this.html.innerText;
-    }
-    set size(value){
-        this.html.style.fontSize = value+"px";
-    }
-    get size(){
-        return this.html.style.fontSize;
-    }
-    set color(value){
-        this.html.style.color = value;
-    }
-    get color(){
-        return this.html.style.color;
-    }
-    set background(value){
-        this.html.style.background = value;
-    }
-    get background(){
-        return this.html.style.background;
-    }
-    set radius(value){
-        this.html.style.borderRadius = value+"px";
-    }
-    get raduis(){
-        return this.html.style.borderRadius;
-    }
-    set topLeftRadius(value){
-        this.html.style.borderTopLeftRadius = value;
-    }
-    get topLeftRadius(){
-        return this.html.style.borderTopLeftRadius;
-    }
-    set topRightRadius(value){
-        this.html.style.borderTopRightRadius = value;
-    }
-    get topRightRadius(){
-        return this.html.style.borderTopRightRadius;
-    }
-    set bottomLeftRadius(value){
-        this.html.style.borderBottomLeftRadius = value;
-    }
-    get bottomLeftRadius(){
-        return this.html.style.borderBottomLeftRadius;
-    }
-    set bottomRightRadius(value){
-        this.html.style.borderBottomRightRadius = value;
-    }
-    get bottomRightRadius(){
-        return this.html.style.borderBottomRightRadius;
-    }
-    set lineHeight(value){
-        this.html.style.lineHeight = value+"px";
-    }
-    get lineHeight(){
-        return this.html.style.lineHeight;
-    }
-    set align(value){
-        this.html.style.textAlign = value;
-    }
-    get align(){
-        return this.html.style.textAlign;
-    }
-    set alpha(value){
-        this.html.style.opacity = value;
-    }
-    get alpha(){
-        return this.html.style.opacity;
-    }
-    set overflow(value){
-        this.html.style.overflow = value?"visible":'hidden';
-    }
-    get overflow(){
-        return this.html.style.overflow;
-    }
-    set boxShadow(value){
-        this.html.style.boxShadow = value;
-    }
-    get boxShadow(){
-        return this.html.style.boxShadow;
-    }
-    set border(value){
-        this.html.style.border = value;
-    }
-    get border(){
-        return this.html.style.border;
-    }
-    set borderTop(value){
-        this.html.style.borderTop = value;
-    }
-    get borderTop(){
-        return this.html.style.borderTop;
-    }
-    set borderBottom(value){
-        this.html.style.borderBottom = value;
-    }
-    get borderBotton(){
-        return this.html.style.borderBottom;
-    }
-    set borderLeft(value){
-        this.html.style.borderLeft = value;
-    }
-    get borderLeft(){
-        return this.html.style.borderLeft;
-    }
-    set borderRight(value){
-        this.html.style.borderRight = value;
-    }
-    get borderRight(){
-        return this.html.style.borderRight;
-    }
-    set weight(value){
-        this.html.style.fontWeight = value;
-    }
-    get weight(){
-        return this.html.style.fontWeight;
-    }
-    set cursor(value){
-        this.html.style.cursor = value;
-    }
-    get cursor(){
-        return this.html.style.borderBottom;
-    }
-    set top(value){
-        value = typeof value == "number"?value+"px":value;
-        this.html.style.top = value;
-    }
-    get top(){
-        return this.html.style.top;
-    }
-    set left(value){
-        value = typeof value == "number"?value+"px":value;
-        this.html.style.left = value;
-    }
-    get left(){
-        return this.html.style.left;
-    }
-    set right(value){
-        value = typeof value == "number"?value+"px":value;
-        this.html.style.right = value;
-    }
-    get right(){
-        return this.html.style.right;
-    }
-    set bottom(value){
-        value = typeof value == "number"?value+"px":value;
-        this.html.style.bottom = value;
-    }
-    get bottom(){
-        return this.html.style.bottom;
-    }
-    set anchor(value){
-        this.html.style.transform = `translate(-${value.x*100}%,-${value.y*100}%)`;
-    }
-    get anchor(){
-        return this.html.style.transform;
-    }
-
 
     Awake(){
-        Render.AddUI(this.html);
+        super.Awake();
     }
 
     OnEnable(){
-        this.html.style.display = "block";
+        super.OnEnable();
     }
 
     OnDisable(){
-        this.html.style.display = "none";
+        super.OnDisable();
     }
 
     OnDestroy(){
-        Render.RemoveUI(this.html);
+        super.OnDestroy();
     }
 }
